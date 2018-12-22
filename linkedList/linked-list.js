3a00ebfd5ba581e8a849d696300be6d273bdb87f
Heather Cherewaty <hr.cherewaty@gmail.com>
2018-12-22 14:35:55 -0800

    Adds singly linked list with insert, includes, and print methods.  also includes three test assertions

--- linkedList/linked-list.js
+++ linkedList/linked-list.js
@@ -2,25 +2,72 @@
 
 const util = require('util');
 
-let linkList = module.exports = {};
+// Node class
+class Node {
+    constructor(value) {
+        this.value = value;         // value stored in Node
+        this.next = null;          // pointer to the next Node
+    }
+}
 
-// class node {
-//     constructor(value) {
-//         this.value = value;
-//         this.next = null;
-//     }
-// }
+// LinkedList class
+class LinkedList {
+    constructor() {
+        this.head = null;           // declare head Node as null
+    }
 
-// class LinkedList {
-//     constructor() {
-//         this.head = null;
-//     }
+    // Add a new Node with value to the head
+    insert(value) {
+        let node = new Node(value);       // adds a new Node
+        if (! this.head) {                // if there is no head...
+            this.head = node;             // assign this node to head
+            return;
+        }
 
-//     add(value) {
-//         let node = new Node(value);
-//         if (! this.head) {
-//             this.head = node;
-//             return;
-//         }
-//     }
-// }
+        let current = this.head;          // start at head Node...
+
+        while(current.next) {             // while I have a next on the thing I'm looking at...
+        
+        current = current.next;           // if I have a next, change pointer to the next one
+        }
+
+        current.next = node;              // when done with while loop, will be at the last one so add a node, node has a value of null
+    }
+
+    // Check if value exists in list
+    includes(value) {                     // returns boolean result depending on whether 
+        
+        let current = this.head;
+        // console.log(current);
+        while(current.next) {
+            if(current.value === value) {    // if value is value, return true...
+                // console.log(true);
+                return true;
+            } 
+            current = current.next;       // Keep moving through the list to the next node
+        } 
+        return current.value === value ? true: null; // checking the last node for the value I'm searching for; if it's true, return true, else return null                       
+    }
+
+    print() {                             // outputs all current Node values
+        return list;
+    }
+}
+
+
+
+let list = new LinkedList();
+
+// list.print();
+
+
+list.insert('Heather');
+list.insert('Steve');
+list.insert('Atlas');
+list.insert('Lily');
+
+console.log(list.includes('John'));
+
+// console.log(util.inspect(list, {depth: 10}));
+
+module.exports = LinkedList;
--- linkedList/linked-list.test.js
+++ linkedList/linked-list.test.js
@@ -1,2 +1,30 @@
-const list = require('./linked-list.js');
+'use strict';
 
+const LinkedList = require('./linked-list.js');
+
+describe('includes method', () => {
+    it('can find values at the beginning, middle, and end of linked list', () => {
+        let newList = new LinkedList();
+        newList.insert('Heather');
+        newList.insert('Steve');
+        newList.insert('Atlas');
+        newList.insert('Lily');
+        expect(newList).toBeTruthy();
+    });
+});
+
+describe('insert method', () => {
+    it('creates a new node', () => {
+        let newList = new LinkedList();
+        newList.insert('Heather');
+        expect(newList).toEqual({"head": {"next": null, "value": "Heather"}});
+    });
+});
+
+describe('print method', () => {
+    it('prints a list', () => {
+        let newList = new LinkedList();
+        let value = newList.print();
+        expect(value).toEqual({"head": {"next": {"next": {"next": {"next": null, "value": "Lily"}, "value": "Atlas"}, "value": "Steve"}, "value": "Heather"}});
+    });
+});
